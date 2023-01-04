dcce8097a1e9773072a25563cebeb2ce7e80fe2a
pau.serrano <pauserrano2000@gmail.com>
2023-01-04 17:28:29 +0100

    Routes created

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom";
-import { Layout } from "./Components/Layout/Layout"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Header } from "./Components/Header/Header"
+import { Welcome} from "./Containers/Welcome/Welcome"
+import { LogIn } from "./Containers/LogIn/LogIn"
+import { SignUp } from './Containers/SignUp/SignUp';
+import { Search } from './Containers/Search/Search';
+import { ImageGeneration } from './Containers/ImageGeneration/ImageGeneration';
+import { Detail } from './Containers/Detail/Detail';
+import { Favorites } from './Containers/Favorites/Favorites';
+import { useThemeContext } from "./Context/theme-context";
+import { useAuthContext } from "./Context/auth-context";
 
 function App() {
+  const { Theme } = useThemeContext();
+  const { isLoggedIn } = useAuthContext();
+
   return (
-    <Layout>
-      <Routes>
-        {/*<Route path="/" element={<Home />} />*/}
-      </Routes>
-    </Layout>
+    <div className={`App ${isLoggedIn ? "" : "welcome-"}${Theme}-background`}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          {!isLoggedIn && <>
+            <Route path="/" element={<Welcome />} />
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/signup" element={<SignUp />} />
+          </>}
+          {isLoggedIn && <>
+            <Route path="/search" element={<Search />} />
+            <Route path="/image-generation" element={<ImageGeneration />} />
+            <Route path="/detail" element={<Detail />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </>}
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
