0be5c30fb688c080c9e356030c9103ec7e9e5f91
pau.serrano <pauserrano2000@gmail.com>
2023-01-04 17:26:08 +0100

    Auth context created

---
+++ src/Context/auth-context.tsx
@@ -0,0 +1,49 @@
+import React, { FC, PropsWithChildren, useState, useContext } from "react";
+
+
+type AuthContextObj = {
+    token: string | null;
+    isLoggedIn: boolean;
+    login: (token: string) => void;
+    logout: () => void;
+}
+
+const AuthContext = React.createContext<AuthContextObj>({
+    token: null,
+    isLoggedIn: false,
+    login: (token: string) => { },
+    logout: () => { },
+});
+
+export const AuthContextProvider: FC<PropsWithChildren> = ({ children }) => {
+    const initialToken = localStorage.getItem("token"); //just when the component is mounted
+    const [token, setToken] = useState<string | null>(initialToken);
+    const isLoggedIn = !!token;
+
+    const loginHandler = (token: string) => {
+        setToken(token);
+        localStorage.setItem('token',token);
+    }
+
+    const logoutHandler = () => {
+        setToken(null);
+        localStorage.removeItem('token');
+    }
+
+    const contextValue = {
+        token,
+        isLoggedIn,
+        login: loginHandler,
+        logout: logoutHandler,
+    }
+    
+    return (
+        <AuthContext.Provider value={contextValue}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export const useAuthContext = () => {
+    return useContext(AuthContext)
+};
