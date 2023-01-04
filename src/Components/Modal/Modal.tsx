4c28b2b4776aa951ea4b2ed378aa83d7a4f0f91f
pau.serrano <pauserrano2000@gmail.com>
2023-01-04 17:29:09 +0100

    Modal created

---
+++ src/Components/Modal/Modal.tsx
@@ -0,0 +1,27 @@
+import { createPortal } from 'react-dom';
+import "./Modal.css";
+import { FC, PropsWithChildren} from "react";
+import { Backdrop } from './Backdrop/Backdrop';
+
+type ModalProps = {
+    onClose: () => void
+}
+export const Modal: FC<PropsWithChildren<ModalProps>> = ({ children, onClose }) => {
+
+    const closeHandler = () => {
+        onClose();
+    };
+    return (<>
+        {createPortal(
+            <Backdrop onClose={closeHandler} />,
+            document.getElementById('backdrop-root')!
+        )}
+        {createPortal(
+            <div className='modal'>
+                {children}
+            </div>
+            ,
+            document.getElementById('modal-root')!
+        )}
+    </>);
+};
