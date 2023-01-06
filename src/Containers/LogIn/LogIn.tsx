005a0b0f29986cb2fcf735eb7e070d95e50c5bf0
pau.serrano <pauserrano2000@gmail.com>
2023-01-06 22:50:00 +0100

    Refactor signup and login

--- src/Containers/LogIn/LogIn.tsx
+++ src/Containers/LogIn/LogIn.tsx
@@ -1,24 +1,74 @@
 import "./LogIn.css";
-import { FC, useState } from "react";
+import { FC } from "react";
 import { Modal } from "../../Components/Modal/Modal";
+import { Form } from "../../Components/Form/Form";
+import { useInput } from "../../Hooks/useInput";
+import { useThemeContext } from "../../Context/theme-context";
 import { useAuthContext } from "../../Context/auth-context";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { Callout } from "../../Components/Callout/Callout";
+import { ReactComponent as Logo } from "../../Assets/smallLogo.svg"
+import { validateEmail, validatePassword } from "../../Services/validate";
 
 export const LogIn: FC = () => {
+  const { Theme } = useThemeContext();
   const { login } = useAuthContext();
   const navigate = useNavigate()
+
+  const emailInput = useInput(validateEmail);
+  const passwordInput = useInput(validatePassword);
 
   const loginHandler = () => {
     login("1")
     navigate("/search")
- }
+  }
+
+  let formIsValid = false;
+  if (emailInput.isValid && passwordInput.isValid) {
+    formIsValid = true;
+  }
+
   return (
     <div className="login">
       <Modal onClose={() => navigate("/")}>
-        <h1>Este es el modal de log in<button onClick={loginHandler}>login</button></h1>
-      </Modal>
-      
-
+        <div className={`login-modal ${Theme}-login-modal`}>
+          <Logo className={`small-logo ${Theme}-small-logo`} />
+          <h1 className={`login-title ${Theme}-login-title`}>
+            Log in to ImageHub
+          </h1>
+          <Form>
+            <Form.Input
+              label="Email address"
+              type="email"
+              id="email"
+              value={emailInput.value}
+              onChange={emailInput.ChangeHandler}
+              onBlur={emailInput.BlurHandler}
+              hasError={emailInput.hasError}
+              errorText="E.g. example@example.com"
+            />
+            <Form.Input
+              label="Password"
+              type="password"
+              id="password"
+              value={passwordInput.value}
+              onChange={passwordInput.ChangeHandler}
+              onBlur={passwordInput.BlurHandler}
+              hasError={passwordInput.hasError}
+              errorText="At least 8 characters, with numbers, lowercase and uppercase letters"
+            />
+            <Form.Submit
+              onSubmit={loginHandler}
+              disabled={!formIsValid}
+              text="Sign in"
+            />
+          </Form>
+          <Callout to="/signup" textLink="Create an account">
+            New to ImageHub?
+          </Callout>
+        </div>
+      </Modal >
     </div>
+
   );
 };
--- src/Containers/SignUp/SignUp.tsx
+++ src/Containers/SignUp/SignUp.tsx
@@ -1,25 +1,98 @@
 import "./SignUp.css";
 import { FC, useState } from "react";
 import { Modal } from "../../Components/Modal/Modal";
+import { Form } from "../../Components/Form/Form";
+import { useInput } from "../../Hooks/useInput";
+import { useThemeContext } from "../../Context/theme-context";
 import { useAuthContext } from "../../Context/auth-context";
 import { Navigate, useNavigate } from "react-router-dom";
+import { Callout } from "../../Components/Callout/Callout";
+import { ReactComponent as Logo } from "../../Assets/smallLogo.svg"
+import { validateName, validateEmail, validatePassword } from "../../Services/validate";
 
 export const SignUp: FC = () => {
+  const { Theme } = useThemeContext();
   const { login } = useAuthContext();
   const navigate = useNavigate()
-  
-  const loginHandler = () => {
+
+  const firstNameInput = useInput(validateName);
+  const lastNameInput = useInput(validateName);
+  const emailInput = useInput(validateEmail);
+  const passwordInput = useInput(validatePassword);
+
+  const signupHandler = () => {
     login("1")
     navigate("/search")
- }
+  }
+
+  let formIsValid = false;
+  if (firstNameInput.isValid && lastNameInput.isValid && emailInput.isValid && passwordInput.isValid) {
+    formIsValid = true;
+  }
+
 
   return (
     <div className="signup">
       <Modal onClose={() => navigate("/")}>
-        <h1>Este es el modal de sign up<button onClick={loginHandler}>login</button></h1>
-      </Modal>
-      
+        <div className={`signup-modal ${Theme}-signup-modal`}>
+          <Logo className={`small-logo ${Theme}-small-logo `} />
+          <h1 className={`signup-title ${Theme}-signup-title`}>
+            Sign up to ImageHub
+          </h1>
+          <Form>
+            <Form.Input
+              label="First name"
+              type="text"
+              id="first"
+              value={firstNameInput.value}
+              onChange={firstNameInput.ChangeHandler}
+              onBlur={firstNameInput.BlurHandler}
+              hasError={firstNameInput.hasError}
+              errorText="Numbers and some special characters not allowed"
+            />
+            <Form.Input
+              label="Last name"
+              type="text"
+              id="last"
+              value={lastNameInput.value}
+              onChange={lastNameInput.ChangeHandler}
+              onBlur={lastNameInput.BlurHandler}
+              hasError={lastNameInput.hasError}
+              errorText="Numbers and some special characters not allowed"
+            />
+            <Form.Input
+              label="Email address"
+              type="email"
+              id="email"
+              value={emailInput.value}
+              onChange={emailInput.ChangeHandler}
+              onBlur={emailInput.BlurHandler}
+              hasError={emailInput.hasError}
+              errorText="E.g. example@example.com"
+            />
+            <Form.Input
+              label="Password"
+              type="password"
+              id="password"
+              value={passwordInput.value}
+              onChange={passwordInput.ChangeHandler}
+              onBlur={passwordInput.BlurHandler}
+              hasError={passwordInput.hasError}
+              errorText="At least 8 characters, with numbers, lowercase and uppercase letters"
+            />
+            <Form.Submit
+              onSubmit={signupHandler}
+              disabled={!formIsValid}
+              text="Sign up"
+            />
+          </Form>
+          <Callout to="/login" textLink="Log in">
+            Already have an account?
+          </Callout>
+        </div>
+      </Modal >
 
-    </div>
+
+    </div >
   );
 };
