df62b76cba1f17ff5573f5fd626eeb5ba11a1389
pau.serrano <pauserrano2000@gmail.com>
2023-01-07 14:11:24 +0100

    Welcome content added

--- src/Containers/Welcome/Welcome.tsx
+++ src/Containers/Welcome/Welcome.tsx
@@ -7,8 +7,16 @@ export const Welcome: FC = () => {
 
   return (
     <main className="welcome">
-      <h1 className={`heading ${Theme}-heading `}>
-        WELCOMEEEEEE ALI </h1>
+      <h1 className={`headline ${Theme}-headline`}>
+        Find the Perfect Image for Your Design with ImageHub
+      </h1>
+      <p className={`overview ${Theme}-overview`}>
+        Welcome to ImageHub, the ultimate destination for finding the perfect image for your design projects.
+        Our wide selection of high-quality, free, copyright-free stock images makes it easy to find exactly what
+        you need. And if you can't find exactly what you're looking for, our AI-powered generators allow you to
+        create custom images on the fly. Plus, with our favorites feature, you can easily save and download
+        your favorite images for later use. Elevate your designs with ImageHub.
+      </p>
     </main>
   );
 };
