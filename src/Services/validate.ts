8fa5535cb0d585e24793b79db210c05b2e5e682d
pau.serrano <pauserrano2000@gmail.com>
2023-01-08 23:51:02 +0100

    Validation change to include ñ

--- src/Services/validate.ts
+++ src/Services/validate.ts
@@ -1,19 +1,20 @@
-// It does not allow numbers, symbols, or other special characters to be included in the name.
+// Checks if the string contains only letters, and allows for spaces, hyphens, and some
+// punctuation. It does not allow numbers, symbols, or other special characters.
 export const validateName = (value: string): boolean => {
-    const nameRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
-    return nameRegex.test(value);
+  const nameRegex = /^[a-zA-Zñ]+(([',. -][a-zA-Zñ ])?[a-zA-Zñ]*)*$/;
+  return nameRegex.test(value);
 };
 
-// e. g. test@example.com
+// Checks if the string is a properly formatted email address, including the local part
+// (before the @) and the domain part (after the @) e. g. test@example.com.
 export const validateEmail = (value: string): boolean => {
   const emailRegex =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z0-9\-]+\.)+[a-zA-Z]{2,}))$/;
   return emailRegex.test(value);
 };
 
-// At least 8 characters and at least 1 uppercase letter, 1 lowercase letter and 1 number
+// At least 8 characters and at least 1 uppercase letter, 1 lowercase letter and 1 number.
 export const validatePassword = (value: string): boolean => {
-  const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+  const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\dñ]{8,}$/;
   return passwordRegex.test(value);
 };
-
