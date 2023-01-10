c6b0d30b963449daab1f6692e0471fdca1730f72
pau.serrano <pauserrano2000@gmail.com>
2023-01-10 17:00:18 +0100

    Api calls json server added

--- src/Services/apicalls.ts
+++ src/Services/apicalls.ts
@@ -1,26 +1,65 @@
 import axios from "axios";
 
+const JSON_SERVER_API_URL = "http://localhost:3000";
 
-export const getToken = async (email: string, password:string  ) => {
-    const token = "999"
-    return token ;
-  };
+export const getAuthToken = async (
+  email: string,
+  password: string
+): Promise<string | null> => {
+  try {
+    const response = await axios.get(`${JSON_SERVER_API_URL}/users`, {
+      params: { email, password },
+    });
+    if (response.data.length > 0) {
+      return response.data[0].id.toString();
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
 
-const DALL_E_API_ENDPOINT = 'https://api.openai.com/v1/images/generations';
+type UserData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+};
+
+export const getUserData = async (token: string): Promise<UserData | null> => {
+  try {
+    const response = await axios.get(`${JSON_SERVER_API_URL}/users/${token}`);
+    if (response.data !== null) {
+      return {
+        firstName: response.data.firstName,
+        lastName: response.data.lastName,
+        email: response.data.email,
+      };
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+const DALL_E_API_URL = "https://api.openai.com/v1/images/generations";
 
 export const generateImageFromDescription = async (description: string) => {
-  const response = await axios.post(DALL_E_API_ENDPOINT, {
+  const response = await axios.post(DALL_E_API_URL, {
     prompt: description,
-    model: 'image-alpha-001',
-    api_key: 'YOUR_API_KEY_HERE',
+    model: "image-alpha-001",
+    api_key: "YOUR_API_KEY_HERE",
   });
   return response.data.data.url;
 };
 
-const API_URL = 'https://api.unsplash.com';
+const UNSPLASH_API_URL = "https://api.unsplash.com";
 
 export const searchPhotos = (query: string) => {
-  return axios.get(`${API_URL}/search/photos`, {
+  return axios.get(`${UNSPLASH_API_URL}/search/photos`, {
     params: { query },
     headers: {
       Authorization: `Client-ID YOUR_ACCESS_KEY`,
