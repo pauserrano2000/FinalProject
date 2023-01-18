c47bbc24a7403e85da7298637ef41ae5edde3873
pau.serrano <pauserrano2000@gmail.com>
2023-01-19 00:46:48 +0100

    Util camelCaseToWords added

--- src/Utils/utils.ts
+++ src/Utils/utils.ts
@@ -1,8 +1,19 @@
 export const capitalizeFirstLetter = (string: string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
 
-export const getInitials = (firstName: string | null, lastName:string | null) => {
-    return (firstName && firstName?.charAt(0) + (lastName && lastName?.charAt(0))) ?? null;
-    
-}
+export const getInitials = (
+  firstName: string | null,
+  lastName: string | null
+) => {
+  return (
+    (firstName && firstName?.charAt(0) + (lastName && lastName?.charAt(0))) ??
+    null
+  );
+};
+
+export const camelCaseToWords = (str: string) => {
+  return str.replace(/([A-Z])/g, " $1").replace(/^./, function (str) {
+    return str.toUpperCase();
+  });
+};
