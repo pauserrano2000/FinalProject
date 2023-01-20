079a66030f04fd9c5c2034e47a7f4bc448a397e6
pau.serrano <pauserrano2000@gmail.com>
2023-01-20 15:55:13 +0100

    Profile settings change structure and styling

--- src/Containers/ProfileSettings/ProfileSettings.tsx
+++ src/Containers/ProfileSettings/ProfileSettings.tsx
@@ -21,9 +21,15 @@ export const ProfileSettings: FC = () => {
   const firstNameInput = useInput(validateName);
   const lastNameInput = useInput(validateName);
   const emailInput = useInput(validateEmail);
-  const passwordInput = useInput(validatePassword);
+  const passwordInput1 = useInput(validatePassword);
+  const passwordInput2 = useInput(validatePassword);
 
-  const updateHandler = async (event: React.FormEvent<HTMLFormElement>, key: keyof UpdateUserData, input: Input) => {
+  let formPasswordIsValid = false;
+  if (passwordInput1.isValid && passwordInput2.isValid && passwordInput1.value === passwordInput2.value) {
+    formPasswordIsValid = true;
+  }
+
+  const updateHandler = async (event: React.FormEvent<HTMLFormElement>, key: keyof UpdateUserData, input: Input, altReset?: () => void) => {
     event.preventDefault();
     const update: UpdateUserData = {}
     if (key === "firstName" || key === "lastName") {
@@ -34,6 +40,9 @@ export const ProfileSettings: FC = () => {
     try {
       const isUpdated = await updateUser(token!, update);
       input.reset();
+      if (altReset) {
+        altReset();
+      }
       if (isUpdated) {
         resetUserData();
         showSuccesNotification({
@@ -58,32 +67,36 @@ export const ProfileSettings: FC = () => {
 
   return (
     <main className="profile-settings">
-      <div className="profile-settings-wrapper">
-        <div className="user-wrapper">
-          <Avatar size={200} />
-          <div>
-            <h1>
-              {`${firstName} ${lastName}`}
-            </h1>
-            <h2>
-              {email}
-            </h2>
-          </div>
+      <div className={`profile-settings__user ${theme}-profile-settings__user`}>
+        <Avatar size={170} radius={23} />
+        <div className="profile-settings__user__info">
+          <p className="profile-settings__user__info__name">
+            {`${firstName} ${lastName}`}
+          </p>
+          <p className="profile-settings__user__info__email">
+            {email}
+          </p>
         </div>
-        <div className="form-wrapper">
+      </div>
+      <div className="profile-settings__forms">
+        <div className="profile-settings__forms__info">
+          <h2 className={`profile-settings__forms__heading ${theme}-profile-settings__forms__heading`}>
+            Edit personal info
+          </h2>
           <Form direction="row" onSubmit={(event) => updateHandler(event, "firstName", firstNameInput)}>
             <Form.Input
               label="First name"
               type="text"
               id="firstName"
               value={firstNameInput.value}
+              placeholder={firstName!}
               onChange={firstNameInput.changeHandler}
               onBlur={firstNameInput.blurHandler}
               hasError={firstNameInput.hasError}
               errorText="Numbers and some special characters not allowed"
             />
             <Form.Submit disabled={!firstNameInput.isValid}>
-              <IconEdit size={22}/>
+              <IconEdit size={22} />
             </Form.Submit>
           </Form>
           <Form direction="row" onSubmit={(event) => updateHandler(event, "lastName", lastNameInput)}>
@@ -92,6 +105,7 @@ export const ProfileSettings: FC = () => {
               type="text"
               id="lastName"
               value={lastNameInput.value}
+              placeholder={lastName!}
               onChange={lastNameInput.changeHandler}
               onBlur={lastNameInput.blurHandler}
               hasError={lastNameInput.hasError}
@@ -107,28 +121,44 @@ export const ProfileSettings: FC = () => {
               type="email"
               id="email"
               value={emailInput.value}
+              placeholder={email!}
               onChange={emailInput.changeHandler}
               onBlur={emailInput.blurHandler}
               hasError={emailInput.hasError}
               errorText="E.g. example@example.com"
             />
             <Form.Submit disabled={!emailInput.isValid}>
-              <IconEdit size={22}/>
+              <IconEdit size={22} />
             </Form.Submit>
           </Form>
-          <Form direction="row" onSubmit={(event) => updateHandler(event, "password", passwordInput)}>
+        </div>
+        <div className="profile-settings__forms__password">
+          <h2 className={`profile-settings__forms__heading ${theme}-profile-settings__forms__heading`}>
+            Change password
+          </h2>
+          <Form onSubmit={(event) => updateHandler(event, "password", passwordInput1, passwordInput2.reset)}>
             <Form.Input
-              label="Password"
               type="password"
               id="password"
-              value={passwordInput.value}
-              onChange={passwordInput.changeHandler}
-              onBlur={passwordInput.blurHandler}
-              hasError={passwordInput.hasError}
+              value={passwordInput1.value}
+              placeholder={"Introduce the new password"}
+              onChange={passwordInput1.changeHandler}
+              onBlur={passwordInput1.blurHandler}
+              hasError={passwordInput1.hasError}
               errorText="Include numbers and capital letters (8+ characters)"
             />
-            <Form.Submit disabled={!passwordInput.isValid}>
-              <IconEdit size={22}/>
+            <Form.Input
+              type="password"
+              id="password"
+              value={passwordInput2.value}
+              placeholder={"Introduce it again"}
+              onChange={passwordInput2.changeHandler}
+              onBlur={passwordInput2.blurHandler}
+              hasError={(passwordInput1.value !== passwordInput2.value) && (!passwordInput1.hasError)}
+              errorText="Check again if it's the same password"
+            />
+            <Form.Submit disabled={!formPasswordIsValid}>
+              <IconEdit size={22} />
             </Form.Submit>
           </Form>
         </div>
