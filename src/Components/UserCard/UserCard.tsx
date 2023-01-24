24f0bbb8059a1a46ab27e4ac5891cfcc06a358dd
pau.serrano <pauserrano2000@gmail.com>
2023-01-24 23:56:20 +0100

    User card created

--- src/Components/UserCard/UserCard.tsx
+++ src/Components/UserCard/UserCard.tsx
@@ -1,18 +1,28 @@
 import { FC } from 'react';
-import { UserDataFE } from '../../Services/apicalls-mapper';
 import './UserCard.css';
+import { UserDataFE } from '../../Services/apicalls-mapper';
+import { useThemeContext } from "../../Context/theme-context";
+import { Avatar } from '../Avatar/Avatar';
 
 type UserCardProps = {
     user: UserDataFE;
 }
 
 export const UserCard: FC<UserCardProps> = ({ user }) => {
-
+    const { theme } = useThemeContext();
     return (
-        <li className="user-card">
-            <p className="user-card__info">
-               {user.firstName+user.lastName+user.favorites.length+user.email+user.avatar}
+        <li className={`user-card ${theme}-user-card`}>
+            <Avatar user={user} size={70} radius={15} />
+            <p className={`user-card__p ${theme}-user-card__p`}>
+                {`Full name: ${user.firstName} ${user.lastName}`}
             </p>
+            <p className={`user-card__p ${theme}-user-card__p`}>
+                {`Email: ${user.email}`}
+            </p>
+            <p className={`user-card__p ${theme}-user-card__p`}>
+                {`Total favorites: ${user.favorites.length}`}
+            </p>
+
         </li>
     );
 };
